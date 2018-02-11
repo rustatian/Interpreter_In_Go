fc5a6e38b987f94d34e2dacb58e111be3fec6bbe
Valery Piashchynski <piashchynski_valery@hotmail.com>
2018-02-11 15:32:28 +0300

    Test passed

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -57,6 +57,10 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -66,11 +70,23 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
 	}
+}
 
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
 
 //Reads in an identifier and advances our lexer's position until it encounters a non-letter character
