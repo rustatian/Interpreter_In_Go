6347df5f72309e951e05e0013344d0d5216dc66c
Valery Piashchynski <piashchynski_valery@hotmail.com>
2018-02-18 22:21:55 +0300

    Add new keywords

--- token/token.go
+++ token/token.go
@@ -38,11 +38,21 @@ const (
 	//Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 //Checks the keywords table to see whether the given identifier is in a fact keyword
