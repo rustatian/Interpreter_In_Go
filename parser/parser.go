e6c18e9e0a58f51f69f18cebda764bfcfc559e83
Valery Piashchynski <piashchynski_valery@hotmail.com>
2018-03-13 16:52:55 +0300

    Update parser Add tests

--- parser/parser.go
+++ parser/parser.go
@@ -1,15 +1,18 @@
 package parser
 
 import (
+	"github.com/ValeryPiashchynski/InterpreterInGo/ast"
 	"github.com/ValeryPiashchynski/InterpreterInGo/lexer"
 	"github.com/ValeryPiashchynski/InterpreterInGo/token"
 )
 
 type Parser struct {
-	//Pointer to the instance
+	//Pointer to the instance of the lexer on which we repeatedly call NextToken() to get the next token
 	l *lexer.Lexer
 
-	curToken  token.Token
+	//pointer to position
+	curToken token.Token
+	//pointer to readPosition
 	peekToken token.Token
 }
 
@@ -26,4 +29,69 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{
+		Token: p.curToken,
+	}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	//TODO We're skipping the expression until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
