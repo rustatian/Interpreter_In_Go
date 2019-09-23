0c793bc319ae92dcabc48eafffe04a253bf39fd1
Valery Piashchynski <piashchynski_valery@hotmail.com>
2019-09-23 20:37:51 +0300

    working on ast

---
+++ ast/ast.go
@@ -0,0 +1,44 @@
+package ast
+
+import "github.com/ValeryPiashchynski/InterpreterInGo/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
